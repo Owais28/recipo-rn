c93b037eb8fc6e59a581c8d2bd25492c090ca5ce
Owais Athar <owaisathar2882@gmail.com>
2023-06-05 20:04:50 +0530

    some changes

--- android/app/src/release/java/com/owais28/taskcardrn/ReactNativeFlipper.java
+++ android/app/src/release/java/com/owais28/taskcardrn/ReactNativeFlipper.java
@@ -4,7 +4,7 @@
  * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
  * directory of this source tree.
  */
-package com.taskcardrn;
+package com.owais28.taskcardrn;
 
 import android.content.Context;
 import com.facebook.react.ReactInstanceManager;
